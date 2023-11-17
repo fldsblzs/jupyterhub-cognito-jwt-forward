c8c2155acdbe8b31acc4d3da654c2361545d966f
Balázs Földesi <foldesibalazs@gmail.com>
2023-11-17 14:39:22 +0100

    Simplify package to parse jwt and set email

--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
 from setuptools import setup
 
 setup(
-    name='jupyterhub-jwtauthenticator',
-    version='0.2-dev',
-    description='JSONWebToken Authenticator for JupyterHub',
-    url='https://github.com/mogthesprog/jwtauthenticator',
-    author='mogthesprog',
-    author_email='mevanj89@gmail.com',
+    name='jupyterhub-cognito-jwt-forward',
+    version='0.2',
+    url='https://github.com/fldsblzs/jwtauthenticator',
+    author='fldsblzs',
+    author_email='foldesibalazs@gmail.com',
     license='Apache 2.0',
-    tests_require = [
-    'unittest2',
-    ],
-    test_suite = 'unittest2.collector',
-    packages=['jwtauthenticator'],
+    packages=['jupyterhub-cognito-jwt-forward'],
     install_requires=[
         'jupyterhub',
         'python-jose',
+        'jwt',
+        'requests'
     ]
 )
